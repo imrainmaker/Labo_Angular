5192acc3908ad3bacc94b85af2b9856d0c397383
imrainmaker <bazinjeremy@gmail.com>
2023-05-23 16:08:03 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserProfilComponent } from './components/user-profil/user-profil.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: UserLoginComponent},
+  { path: 'register', component: UserRegisterComponent},
+  { path: 'profil/:id', component: UserProfilComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
