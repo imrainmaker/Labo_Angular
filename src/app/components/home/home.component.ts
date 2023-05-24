c38e1a9284e5b83ac5996d17f7c5b8715b7d2ddb
imrainmaker <bazinjeremy@gmail.com>
2023-05-24 11:04:18 +0200

    navbar & get

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { APIService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  users!: User[];
+  users$!: Observable<User[]>
+
+  constructor(private _APIService: APIService){}
+
+  ngOnInit(): void {
+    
+    this.users$ = this._APIService.getAll();
+
+  }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map, switchMap } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
 
-  constructor() { }
+  users: User[] = [];
+  urlAPI: string = "https://localhost:7115/api/User"
+
+  constructor(private _http: HttpClient) {}
+
+  getAll() : Observable<User[]>{
+    return this._http.get<User[]>(this.urlAPI)
+  };
 }
